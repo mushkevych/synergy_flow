8af90b51f6ebd7fb79ffc44b4204ec524d9fc948
Bohdan Mushkevych <mushkevych@gmail.com>
2016-06-23 00:06:35 -0700

    ongoing integration of D3

--- flow/mx/js/flow_viewer.js
+++ flow/mx/js/flow_viewer.js
@@ -2,11 +2,8 @@
 
 
 function render_flow(data, element) {
-    var width = 960,
-        height = 2200;
-
-    var cluster = d3.layout.cluster()
-        .size([height, width - 160]);
+    var width = 1278,
+        height = 1024;
 
     var diagonal = d3.svg.diagonal()
         .projection(function (d) {
@@ -19,38 +16,49 @@ function render_flow(data, element) {
         .append("g")
         .attr("transform", "translate(40,0)");
 
+    var force = d3.layout.force()
+        .gravity(0.05)
+        .distance(100)
+        .charge(-100)
+        .size([width, height]);
 
-    var nodes = cluster.nodes(data),
-        links = cluster.links(nodes);
+    force.nodes(data.nodes)
+        .links(data.links)
+        .start();
 
     var link = svg.selectAll(".link")
-        .data(links)
-        .enter().append("path")
+        .data(data.links)
+        .enter().append("path")  // vs line; path should give us nice curves
         .attr("class", "link")
         .attr("d", diagonal);
 
     var node = svg.selectAll(".node")
-        .data(nodes)
+        .data(data.nodes)
         .enter().append("g")
         .attr("class", "node")
         .attr("transform", function (d) {
             return "translate(" + d.y + "," + d.x + ")";
         });
+        //.call(force.drag);
 
     node.append("circle")
-        .attr("r", 4.5);  // 10.0
+        .attr("r", 10.0);
 
     node.append("text")
-        .attr("dx", function (d) {
-            return d.children ? -8 : 8;
-        })
-        .attr("dy", 3)
-        .style("text-anchor", function (d) {
-            return d.children ? "end" : "start";
-        })
+        .attr("dx", 12)
+        .attr("dy", ".35em")
         .text(function (d) {
             return d.name;
         });
 
-    d3.select(self.frameElement).style("height", height + "px");
+    force.on("tick", function () {
+    link.attr("x1", function(d) { return d.source.x; })
+        .attr("y1", function(d) { return d.source.y; })
+        .attr("x2", function(d) { return d.target.x; })
+        .attr("y2", function(d) { return d.target.y; });
+
+        node.attr("transform", function (d) {
+            return "translate(" + d.x + "," + d.y + ")";
+        });
+    });
 }
