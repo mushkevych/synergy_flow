234ff2ca4f6e597056270ddc9a107382bcd11450
Bohdan Mushkevych <mushkevych@gmail.com>
2019-11-20 16:42:21 -0800

    .travis.yml updates

--- settings.py
+++ settings.py
@@ -1,7 +1,7 @@
 # module is required to initialized synergy.conf and run unit tests
 
 settings = dict(
-    log_directory='/tmp/logs/synergy-flow',
+    log_directory='/var/log/synergy-flow',
     debug=True,                # if True, logger.setLevel is set to DEBUG. Otherwise to INFO
 
     under_test=True            # marks execution of the Unit Tests
